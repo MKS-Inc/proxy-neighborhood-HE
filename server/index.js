94d879c4052bb2b27816debdc6cbc90102f33dd0
Harrison Edwards <harrisonedwards@Harrisons-MacBook-Pro.local>
2020-04-16 20:16:31 -0700

    syntax corrected on neighborhood service, monthlyCost added to proxy

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const port = 4000;
 
 // neighborhood service
 app.use('/api/neighborhood/:id', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
-app.put('/api/neighborhood/:id', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
-app.post('/api/likes/:user_id/:house', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
-app.delete('/api/likes/:user_id/:house_id', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
-app.put('/api/users/:user_name', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
+app.use('/api/neighborhood/:id', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
+app.use('/api/likes/:user_id/:house', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
+app.use('/api/likes/:user_id/:house_id', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
+app.use('/api/users/:user_name', createProxyMiddleware({ target: 'http://ec2-3-101-23-93.us-west-1.compute.amazonaws.com:3000', changeOrigin: true }));
 
 // monthlyCost_rentalValue
+
+app.use('/homes/:zipcode', createProxyMiddleware({ target: 'http://ec2-52-8-92-61.us-west-1.compute.amazonaws.com/', changeOrigin: true }));
 
 
 // carousel
